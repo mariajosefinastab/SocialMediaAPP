a3a2800f225a27526048f346d308b4f145aea59d
mariajosefinastab <mariajosefinastabile@gmail.com>
2025-01-25 12:14:18 -0300

    Functions

---
+++ script.js
@@ -0,0 +1,38 @@
+/* fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json)) */
+
+const urlBase = 'https://jsonplaceholder.typicode.com/posts'
+let posts = [] //inicia posteos como array vacío
+
+function getData(){
+    fetch(urlBase)
+    .then(res => res.json())
+    .then(data => {
+        posts = data
+        //to do: mpetodo para mostrar info en pantalla
+    })
+    .catch(error => console.error('Error al mostrar datos'))
+}
+
+function renderPostList(){
+    const postList = document.getElementById('postList');
+    postList.innerHTML = '';
+
+    posts.forEach(post => {
+        const listItems = document.createElement('li');
+        listItem.classList.add('postItem');
+        listItem.innerHTML = `
+
+        <strong>${post.title}</strong>
+        <p>${post.body}</p>
+        <button onclick = "editPost(${post.id})">Editar</button>
+        <button onclick = "deletePost(${post.id})">Eliminar</button>
+
+        <div id='editForm-${post.id}' class='editForm' style='display:none'>
+            <label for='editTitle'>Título: </label>
+            <input type='text' id='editTitle'-${post.id}' value='${post.title}' required>
+        </div>
+        `
+    })
+}
